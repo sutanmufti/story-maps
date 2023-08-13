7356823f63eacd43fb953f2f4507e953feef9a85
sutan mufti <upsutan@gmail.com>
2023-08-13 10:47:45 +0100

    new scale

---
+++ _app/immutable/chunks/paths.abb34da7.js
@@ -0,0 +1,1 @@
+var s;const l=((s=globalThis.__sveltekit_1llku0r)==null?void 0:s.base)??"/story-maps";var a;const t=((a=globalThis.__sveltekit_1llku0r)==null?void 0:a.assets)??l;export{t as a,l as b};
